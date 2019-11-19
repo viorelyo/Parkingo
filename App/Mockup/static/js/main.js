0018741f10c0b25a99f9e283a0f91469ece8a2ea
viorelyo <viorelgurdis@gmail.com>
2019-11-20 01:07:53 +0200

    Upload of frame & dataFetch added

---
+++ App/Mockup/static/js/main.js
@@ -0,0 +1,27 @@
+function loadTable() {
+    fetch('/spots', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    .then(data => data.json())
+    .then(data => {
+        console.log(data);
+    })
+}
+
+
+function uploadFrame() {
+    console.log("pls");
+    var input = document.querySelector('input[type="file"]');
+
+    const data = new FormData();
+    data.append('file', input.files[0]);
+    data.append('filename', 'example');
+ 
+    fetch('/upload', {
+        method: 'POST',
+        body: data
+    });
+}
